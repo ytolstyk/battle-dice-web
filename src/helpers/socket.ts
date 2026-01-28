30969c786b2dcc3d424d7fc1ee509a5e5b890dd4
Yuriy Tolstykh <yuriy.tolstykh@gmail.com>
2026-01-28 10:55:13 -0800

    fighting cors

--- src/helpers/socket.ts
+++ src/helpers/socket.ts
@@ -2,7 +2,7 @@ import { io } from "socket.io-client";
 
 const URL =
   process.env.NODE_ENV === "production"
-    ? "wss://battle-dice-server.onrender.com"
+    ? "wss://battle-dice-server.onrender.com:8080"
     : "ws://localhost:8080";
 
 export const socket = io(URL, { path: "/battle-dice", autoConnect: false });
