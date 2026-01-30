86a779bb1914150ed2018514dbf8a7f904b97f4b
Yuriy Tolstykh <yuriy.tolstykh@gmail.com>
2026-01-30 11:11:19 -0800

    changing render route

--- src/helpers/socket.ts
+++ src/helpers/socket.ts
@@ -2,7 +2,7 @@ import { io } from "socket.io-client";
 
 const URL =
   process.env.NODE_ENV === "production"
-    ? "wss://battle-dice-server.onrender.com:8080"
-    : "ws://localhost:8080";
+    ? "https://battle-dice-server.onrender.com"
+    : "http://localhost:8080";
 
 export const socket = io(URL, { path: "/battle-dice", autoConnect: false });
